d973459a9908e75378d61c26edb050a0c9749258
Dany <dany21sayan_@hotmail.com>
2022-11-19 09:51:34 +0000

    Updates

---
+++ precache-manifest.a6b472eb6a548ad3f104867919c4f706.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b981c5ed60a37c6f63ec8971c7219700",
+    "url": "/react-keeper-app-guided/index.html"
+  },
+  {
+    "revision": "6e514cd2a4e22de3d8b2",
+    "url": "/react-keeper-app-guided/static/js/2.6bb0b902.chunk.js"
+  },
+  {
+    "revision": "872fcbd83aafe2269615",
+    "url": "/react-keeper-app-guided/static/js/main.cf6080f6.chunk.js"
+  },
+  {
+    "revision": "7509a9867eeb761e2e47",
+    "url": "/react-keeper-app-guided/static/js/runtime-main.5e780f3e.js"
+  }
+]);
